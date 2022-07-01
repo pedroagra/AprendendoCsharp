fbacc40b805186fbd7a06498e5a5eaff6828d032
Pedro Agra <agrasoftwares@gmail.com>
2022-06-30 23:28:25 -0300

    Add files via upload

---
+++ P12-CalculaInvestimentoLongoProzo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12_CalculaInvestimentoLongoProzo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 12");
+
+            double valorInvestido = 1000;
+            double fatorRendimento = 1.0036;
+
+            for (int contadorAno = 1; contadorAno <= 5; contadorAno++)
+            {
+                for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+                {
+                    //valorInvestido = valorInvestido * fatorRendimento; equivalente ao código seguinte!
+                    valorInvestido *= fatorRendimento;
+                }
+                fatorRendimento += 0.0010;
+            }
+
+            Console.WriteLine("Ao término do investimento, você terá R$ " + valorInvestido);
+
+            for (int contador = 1; contador <= 10; contador++)
+            {
+                Console.WriteLine(contador);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
